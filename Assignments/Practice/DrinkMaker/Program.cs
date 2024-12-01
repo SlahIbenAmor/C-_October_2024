8d607fdb9aa0c04665490f88a596fbfe5903f1e2
SlaheddineIbenAmor <163729969+SlahIbenAmor@users.noreply.github.com>
2024-12-01 11:43:40 +0100

    assignment

---
+++ Assignments/Practice/DrinkMaker/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿Soda Coke = new Soda("Coca Cola", "brown", 38, 140, false);
+Coffee Cappuccino = new Coffee("French Vanilla", "light brown", 120, 110, "light", "coffee beans");
+Wine Merlot = new Wine("Merlot", "red", 32, 120, "Bordeaux", 1992);
+
+// Create a list of all of our drinks
+List<Drink> AllBeverages = new List<Drink>();
+AllBeverages.Add(Coke);
+AllBeverages.Add(Cappuccino);
+AllBeverages.Add(Merlot);
+
+// Loop through the list and call the ShowDrink for each
+foreach(Drink d in AllBeverages)
+{
+    d.ShowDrink();
+}
+
+// This is invalid because we cannot make instances across child classes
+// Coffee MyDrink = new Soda("Coca Cola", "brown", 38, 140, false);
---
+++ Assignments/Practice/DrinkMaker/Soda.cs
@@ -0,0 +1,14 @@
+class Soda : Drink
+{
+    public bool IsDiet;
+    public Soda(string n, string col, double t, int cal, bool id) : base(n, col, t, true, cal)
+    {
+        IsDiet = id;
+    }
+
+    public override void ShowDrink()
+    {
+        base.ShowDrink();
+        Console.WriteLine($"Is Diet: {IsDiet}");
+    }
+}
