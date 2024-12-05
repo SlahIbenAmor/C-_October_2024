8287456955b5a37413b74114eeaffff71a986251
SlaheddineIbenAmor <163729969+SlahIbenAmor@users.noreply.github.com>
2024-12-05 21:19:01 +0100

    assignment

---
+++ Assignments/Practice/PortfolioII/Controllers/HelloController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioI.Controllers;
+
+public class HelloController : Controller
+
+{
+
+[HttpGet]
+
+[Route("")]
+
+public ViewResult Index()
+
+{
+
+return View("Index");
+
+}
+
+
+[HttpGet("projects")]
+
+public ViewResult Projects()
+
+{
+
+return View("Projects");
+
+}
+
+
+[HttpGet("contact")]
+
+public ViewResult Contact()
+
+{
+
+return View("Contact");
+
+}
+
+}
