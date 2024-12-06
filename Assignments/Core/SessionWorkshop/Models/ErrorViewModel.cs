b7322948e3a5821acd8b1efdb83bbdb1683de13f
SlaheddineIbenAmor <163729969+SlahIbenAmor@users.noreply.github.com>
2024-12-06 09:38:57 +0100

    Delete Assignments/Core/SessionWorkshop directory

--- Assignments/Core/SessionWorkshop/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace SessionWorkshop.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
