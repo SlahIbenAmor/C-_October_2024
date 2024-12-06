965eae33012d5daadb6b44ff887e7747dbae2088
SlaheddineIbenAmor <163729969+SlahIbenAmor@users.noreply.github.com>
2024-12-06 09:42:32 +0100

    assignment

---
+++ Assignments/Core/SessionWorkshop/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SessionWorkshop.Models;
+
+namespace SessionWorkshop.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+       
+    [HttpPost]
+    public IActionResult Register(Player model)
+    {
+         if (ModelState.IsValid)
+        {
+            HttpContext.Session.SetString("UserName", model.Name);
+            return RedirectToAction ("Dashboard");
+        }
+       return View ("Index");
+    }
+
+
+    [HttpPost ("logout")]
+    public IActionResult Logout ()
+    {
+            HttpContext.Session.Clear();
+            return View ("Index");
+    }
+
+    
+    [HttpGet("dashboard")]
+    public IActionResult Dashboard (int CurrentValue)
+    {
+        string? UserName = HttpContext.Session.GetString("UserName");
+        if (UserName == null)
+        {
+            return RedirectToAction ("Index");
+        }
+       
+        return View ();
+    }
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
