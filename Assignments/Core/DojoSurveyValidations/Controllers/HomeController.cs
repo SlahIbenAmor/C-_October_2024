cfd5c446b45ec94f548861d466dfee05db56b894
SlaheddineIbenAmor <163729969+SlahIbenAmor@users.noreply.github.com>
2024-12-06 11:10:40 +0100

    assignment

---
+++ Assignments/Core/DojoSurveyValidations/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyValidations.Models;
+
+namespace DojoSurveyValidations.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+        [HttpPost("register")]
+        public IActionResult Register(Survey ninja)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("DisplayResults", ninja); 
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+
+        public IActionResult DisplayResults(Survey ninja)
+        {
+            return View("Results",ninja); 
+        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Assignments/Core/DojoSurveyValidations/Models/Survey.cs
@@ -0,0 +1,18 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+
+namespace DojoSurveyValidations.Models
+{
+public class Survey 
+{
+  [MinLength(3, ErrorMessage ="Name should be no less than 3 characters")]
+  public string Name {get;set;}
+  public string Location {get;set;}  
+  
+  [Required]
+  public string FavouriteLanguage {get;set;}
+
+  [MinLength(20,ErrorMessage ="comment should be more than 20 characters.")]
+  public string Comment {get;set;}
+}
+}
