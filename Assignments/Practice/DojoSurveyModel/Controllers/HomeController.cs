506f52e560740863c688f00322ba8a90462ce1a1
SlaheddineIbenAmor <163729969+SlahIbenAmor@users.noreply.github.com>
2024-12-19 05:26:53 +0100

    assignment

---
+++ Assignments/Practice/DojoSurveyModel/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyModel.Models;
+
+namespace DojoSurveyModel.Controllers;
+
+public class HomeController : Controller
+{
+
+    static User user;
+
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("process")]
+    public IActionResult Process(User newUser)
+    {
+        user = newUser;
+        return RedirectToAction("Results");
+    }
+
+    [HttpGet("results")]
+    public IActionResult Results()
+    {
+        return View(user);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
