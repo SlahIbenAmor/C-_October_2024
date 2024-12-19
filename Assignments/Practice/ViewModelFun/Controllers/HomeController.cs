d36fddbb121990e0fb6a77d3a8defff91ff6dbb8
SlaheddineIbenAmor <163729969+SlahIbenAmor@users.noreply.github.com>
2024-12-19 05:32:23 +0100

    assignment

---
+++ Assignments/Practice/ViewModelFun/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ViewModelFun.Models;
+
+namespace ViewModelFun.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        string message = "fhdksjhf fhdskfh fhdskjfsgf fgsufdskf fgdskj";
+        return View("Index", message);
+    }
+
+    [HttpGet("numbers")]
+    public IActionResult Numbers()
+    {
+        List<int> SomeNumbers = new List<int>() {1,2,10,21,8,7,3};
+        return View(SomeNumbers);
+    }
+
+    [HttpGet("user")]
+    public IActionResult OneUser()
+    {
+        User MyUser = new User()
+        {
+            FirstName = "Neil",
+            LastName = "Gaiman"
+        };
+        return View("User", MyUser);
+    }
+
+    [HttpGet("users")]
+    public IActionResult Users()
+    {
+        List<User> ListOfUsers = new List<User>()
+        {
+            new User() {FirstName = "Neil", LastName="Gaiman"},
+            new User() {FirstName = "Terry", LastName="Pratchet"},
+            new User() {FirstName = "Jane", LastName="Austen"},
+            new User() {FirstName = "Stephen", LastName="King"},
+            new User() {FirstName = "Mary", LastName="Shelley"},
+        };
+
+        return View(ListOfUsers);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
