d1b121d065b4a5299a25140e3ab74bce19296da6
SlaheddineIbenAmor <163729969+SlahIbenAmor@users.noreply.github.com>
2025-01-13 23:59:42 +0100

    assign

---
+++ Assignments/Core/LoginandRegistration/Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LoginandRegistration.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginandRegistration.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private MyContext _context;
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("users/create")]
+    public IActionResult CreateUser(User newUser)
+    {
+        if(ModelState.IsValid)
+        {
+            PasswordHasher<User> Hasher = new PasswordHasher<User>();
+            newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+            _context.Add(newUser);
+            _context.SaveChanges();
+            HttpContext.Session.SetInt32("UserId", newUser.UserId);
+            return RedirectToAction("Dashboard");
+        } else {
+            return View("Index");
+        }
+    }
+
+    [HttpPost("users/login")]
+    public IActionResult LoginUser(LogUser returningUser)
+    {
+        if(ModelState.IsValid)
+        {
+            User? userInDB = _context.Users.FirstOrDefault(u => u.Email == returningUser.LogEmail);
+            if(userInDB == null)
+            {
+                ModelState.AddModelError("LogEmail", "Invalid login attempt");
+                return View("Index");
+            }
+            PasswordHasher<LogUser> hasher = new PasswordHasher<LogUser>();
+            var result = hasher.VerifyHashedPassword(returningUser, userInDB.Password, returningUser.LogPassword);
+            if(result == 0)
+            {
+                ModelState.AddModelError("LogEmail", "Invalid login attempt");
+                return View("Index");
+            }
+            HttpContext.Session.SetInt32("UserId", userInDB.UserId);
+            return RedirectToAction("Dashboard");
+        } else {
+            return View("Index");
+        }
+    }
+
+    [SessionCheck]
+    [HttpGet("dashboard")]
+    public IActionResult Dashboard()
+    {
+        ViewBag.Error = 0;
+        MyViewModel MyModel = new MyViewModel()
+        {
+            LoggedInUser = _context.Users.FirstOrDefault(u => u.UserId == (int)HttpContext.Session.GetInt32("UserId")),
+        };
+        return View(MyModel);
+    }
+
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
+public class SessionCheckAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        int? userId = context.HttpContext.Session.GetInt32("UserId");
+        if(userId == null)
+        {
+            context.Result = new RedirectToActionResult("Index", "Home", null);
+        }
+    }
+}
---
+++ Assignments/Core/LoginandRegistration/Models/MyViewModel.cs
@@ -0,0 +1,8 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+namespace LoginandRegistration.Models;
+public class MyViewModel
+{
+    public User LoggedInUser {get;set;}
+
+}
